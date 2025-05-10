73d2cf8b4489f07ced23a4d5e1d7a200404f9208
Kunal Singh <singhkunal30@outlook.com>
2025-05-11 04:16:12 +0530

    Add movie project

---
+++ src/appwrite.js
@@ -0,0 +1,48 @@
+import { Client, Databases, ID, Query } from 'appwrite';
+
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const APPWRITE_PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+
+const client = new Client()
+  .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+  .setProject(APPWRITE_PROJECT_ID); // Your project ID
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      // Search for the document with the given search term
+      Query.equal('searchTerm', searchTerm),
+    ]);
+    if(result.documents.length > 0) {
+      const doc = result.documents[0];
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1
+      })
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      })
+    }
+  } catch (error) {
+    console.error('Error updating search count:', error);
+  }
+}
+
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.orderDesc('count'),
+      Query.limit(10),
+    ]);
+    console.log(result);
+    return result.documents;
+  } catch (error) {
+    console.error('Error fetching trending movies:', error);
+  }
+}
